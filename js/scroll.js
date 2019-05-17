030c27b01e3d1adce8c698804bccc9513e9d144f
Ruslan <overko@windowslive.com>
2019-05-17 13:21:40 +0000

    form changes

--- js/scroll.js
+++ js/scroll.js
@@ -6,3 +6,23 @@
     $('body,html').animate({scrollTop: top}, 2000);
     });
 });
+
+$('#contact-form').submit((function(e){
+  var name = $('#client-name');
+  var email = $('#client-email');
+  var message = $('#client-message');
+
+if(name.value == "" || email.value == "" || message == ""){
+  alertify.error('Please check your entries')
+}else{
+  $.ajax({
+    url: "https://formspree.io/overko@windowslive.com",
+    method:"POST",
+    data: $(this).serialize(),
+    dataType: "json"
+  });
+  e.preventDefault()
+  $(this).get(0).reset()
+  alertify.success('Message send')
+}
+}))
